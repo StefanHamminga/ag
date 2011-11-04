8579e665fc84e27270bd54ea9ecb164a9b65dfdd
Stefan Hamminga <stefan@engit.nl>
2011-11-05 00:06:05 +0100

    Check for root, if needed

--- ag.sh
+++ ag.sh
@@ -4,7 +4,7 @@
 ionice -c 3 -p $$ > /dev/null
 renice -n 5 -p $$ > /dev/null
 
-function usage {
+usage() {
 	echo -e "apt-get shortcuts\n"
 	echo "Usage:"
 	echo "$0 {u|i|a|ui|ua|d|t|r|p|s|v|c} [options]"
@@ -25,48 +25,64 @@ function usage {
 # Nasty hack to give the search output colors.
 # This was taken and modified from an example found on some website, if it was
 # you, send me a message and I will properly attribute the part.
-function sedgrep ()
-{
+sedgrep() {
     C_PATT=`echo -e '\033[33;01m'`
     C_NORM=`echo -e '\033[m'`
 
     sed -s "s/$1/${C_PATT}&${C_NORM}/gi"
 }
 
+checkforroot() {
+	if [[ $EUID -ne 0 ]]; then
+		echo "This function must be run as root" 1>&2
+		usage
+		exit 1
+	fi
+}
+
 case "$1" in
 	"u")
+	checkforroot
 	shift
 	apt-get -qq update $@ && echo "Update succesfull"
 	;;
 	"i")
+	checkforroot
 	shift
 	apt-get install $@
 	;;
 	"a")
+	checkforroot
 	shift
 	apt-get install -y --no-remove --install-recommends $@
 	;;
 	"ui")
+	checkforroot
 	shift
 	apt-get -qq update
 	apt-get install $@
 	;;
 	"ua")
+	checkforroot
 	shift
 	apt-get -qq update
 	apt-get install -y --no-remove --install-recommends $@
 	;;
 	"d")
+	checkforroot
+	shift
 	apt-get -qq update && apt-get -qq dist-upgrade && echo "Dist upgrade succesfull"
 	;;
 	"t")
 	apt-get -qq update && echo "The following packages would be upgraded:" && apt-get -s -qq dist-upgrade | grep ^"Inst" | sed s/"^Inst "// | sort
 	;;
 	"r")
+	checkforroot
 	shift
 	apt-get remove $@
 	;;
 	"p")
+	checkforroot
 	shift
 	apt-get remove --purge $@
 	;;
@@ -79,6 +95,7 @@ case "$1" in
 	dpkg-query -W *${@}* | sedgrep $@ | sort
 	;;
 	"c")
+	checkforroot
 	shift
 	apt-get autoremove && apt-get clean
 	;;
